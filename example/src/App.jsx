d407fa27d27b272e45e96b8affbd933fc3836d88
Aaron Lipman <alipman88@gmail.com>
2024-05-12 19:50:14 -0400

    replace example app

---
+++ example/src/App.jsx
@@ -0,0 +1,43 @@
+// example code from https://reactnative.dev/docs/settings
+
+import React, {useState} from 'react';
+import {Button, Settings, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  const [data, setData] = useState(() => Settings.get('data'));
+
+  return (
+    <View style={styles.container}>
+      <Text>Stored value:</Text>
+      <Text style={styles.value}>{data}</Text>
+      <Button
+        onPress={() => {
+          Settings.set({data: 'React'});
+          setData(Settings.get('data'));
+        }}
+        title="Store 'React'"
+      />
+      <Button
+        onPress={() => {
+          Settings.set({data: 'Native'});
+          setData(Settings.get('data'));
+        }}
+        title="Store 'Native'"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  value: {
+    fontSize: 24,
+    marginVertical: 12,
+  },
+});
+
+export default App;
